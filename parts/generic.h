9da3d48f395b1fe5817248f3b2db7a6f58aba548
Tristan Krause <tristan.krause@stud.htwk-leipzig.de>
2019-08-06 15:09:29 +0200

    added warnings for redefinition

--- parts/generic.h
+++ parts/generic.h
@@ -2,30 +2,36 @@
 
 #ifdef HIGH
   #undef HIGH
+  #warning Redefining HIGH...
 #endif
 #define HIGH 1
 
 #ifndef LOW
   #undef LOW
+  #warning Redefining LOW...
 #endif
 #define LOW  0
 
 #ifndef OUT
   #undef OUT
+  #warning Redefining OUT...
 #endif
 #define OUT 1
 
 #ifndef OUTPUT
   #undef OUTPUT
+  #warning Redefining OUTPUT...
 #endif
 #define OUTPUT 1
 
 #ifndef IN
   #undef IN
+  #warning Redefining IN...
 #endif
 #define IN 0
 
 #ifndef INPUT
   #undef INPUT
+  #warning Redefining INPUT...
 #endif
 #define INPUT 0
--- pio/src/pio.h
+++ pio/src/pio.h
@@ -52,26 +52,32 @@
 #include <avr/io.h>
 #ifdef HIGH
 #undef HIGH
+#warning Redefining HIGH...
 #endif
 #define HIGH 1
 #ifndef LOW
 #undef LOW
+#warning Redefining LOW...
 #endif
 #define LOW  0
 #ifndef OUT
 #undef OUT
+#warning Redefining OUT...
 #endif
 #define OUT 1
 #ifndef OUTPUT
 #undef OUTPUT
+#warning Redefining OUTPUT...
 #endif
 #define OUTPUT 1
 #ifndef IN
 #undef IN
+#warning Redefining IN...
 #endif
 #define IN 0
 #ifndef INPUT
 #undef INPUT
+#warning Redefining INPUT...
 #endif
 #define INPUT 0
 #define b(PIN) b_(PIN)
