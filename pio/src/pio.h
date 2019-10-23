0a8c5049e845cd82658b78d3943c1f73d17febc5
Tristan Krause <tristan.krause@stud.htwk-leipzig.de>
2019-10-23 16:18:30 +0200

    fixed bug in gerneric.h

--- pio/src/pio.h
+++ pio/src/pio.h
@@ -26,35 +26,35 @@
 #endif
 #endif
 #define HIGH 1
-#ifndef LOW
+#ifdef LOW
 #undef LOW
 #ifndef PIO_SUPPRESS_REDEFINITION
 #warning Redefining 'LOW' to '0'
 #endif
 #endif
 #define LOW  0
-#ifndef PIO_OUT
+#ifdef PIO_OUT
 #undef PIO_OUT
 #ifndef PIO_SUPPRESS_REDEFINITION
 #warning Redefining 'PIO_OUT' to '1'
 #endif
 #endif
 #define PIO_OUT 1
-#ifndef OUTPUT
+#ifdef OUTPUT
 #undef OUTPUT
 #ifndef PIO_SUPPRESS_REDEFINITION
 #warning Redefining 'OUTPUT' to '1'
 #endif
 #endif
 #define OUTPUT 1
-#ifndef PIO_IN
+#ifdef PIO_IN
 #undef PIO_IN
 #ifndef PIO_SUPPRESS_REDEFINITION
 #warning Redefining 'PIO_IN' to '0'
 #endif
 #endif
 #define PIO_IN 0
-#ifndef INPUT
+#ifdef INPUT
 #undef INPUT
 #ifndef PIO_SUPPRESS_REDEFINITION
 #warning Redefining 'INPUT' to '0'
