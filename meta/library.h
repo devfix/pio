942dd61d0ab1c04321262fe3d23c54d5732f03fe
Tristan Krause <tristan.krause@stud.htwk-leipzig.de>
2020-01-27 21:24:32 +0100

    improved tests with lambdas

--- meta/library.h
+++ meta/library.h
@@ -4,6 +4,16 @@
 #include "concat.hpp"
 
 template<typename ... PINS>
+struct set_ddr {
+    set_ddr() { concat_t<sort_t<build_t<PINS...>>>().ddr_set(); }
+};
+
+template<typename ... PINS>
+struct clear_ddr {
+    clear_ddr() { concat_t<sort_t<build_t<PINS...>>>().ddr_clear(); }
+};
+
+template<typename ... PINS>
 struct set_port {
     set_port() { concat_t<sort_t<build_t<PINS...>>>().port_set(); }
 };
